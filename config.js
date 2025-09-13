bfd9e762f73b2152b00b270deffea78021e19e42
piyashenaarachchilage-cpu <piyashenaarachchilage@gmail.com>
2025-09-13 12:04:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&C8AGwbZK#eSdXpkjntNp2bD2A-Ej5qaGBNpUJLsFfc0s5MkEJe5M' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? '&G0gDEAzB#sqanxvyESbthXR4SsUZJXBlymdSrsohOqESF0XTScbg' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
